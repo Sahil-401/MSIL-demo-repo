8d6e77a09b4f7e1e47205df4b99a8d6696016cdd
Sahil-401 <sahil.singhhr21@gmail.com>
2026-01-02 14:31:07 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ const mysql = require("mysql2");
 
 const app = express();
 
+// Identify environment (DEV / PROD)
+const APP_ENV = process.env.APP_ENV || "UNKNOWN";
+
 const db = mysql.createConnection({
-  host: "mysql",          // K8s service name
+  host: "mysql",        // K8s service name (namespace scoped)
   user: "demo_user",
   password: "demo_pass",
   database: "demo_db"
@@ -15,7 +18,7 @@ db.connect(err => {
     console.error("DB connection failed:", err);
     process.exit(1);
   }
-  console.log("Connected to MySQL");
+  console.log(`Connected to MySQL (${APP_ENV})`);
 
   db.query(`
     CREATE TABLE IF NOT EXISTS users (
@@ -26,13 +29,17 @@ db.connect(err => {
 });
 
 app.get("/", (req, res) => {
-  db.query("INSERT INTO users (name) VALUES ('DEV_USER')", () => {
-    db.query("SELECT * FROM users", (err, rows) => {
-      res.json(rows);
-    });
-  });
+  db.query(
+    "INSERT INTO users (name) VALUES (?)",
+    [APP_ENV],
+    () => {
+      db.query("SELECT * FROM users", (err, rows) => {
+        res.json(rows);
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
-  console.log("App running on port 3000");
+  console.log(`App running in ${APP_ENV} on port 3000`);
 });
