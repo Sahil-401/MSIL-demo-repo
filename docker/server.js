fde2ab95fc8369140a1a2deee51715a8c0c9112a
Sahil-401 <sahil.singhhr21@gmail.com>
2026-01-22 11:12:07 +0530

    Update server.js

--- docker/server.js
+++ docker/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const AUTO_BURN_MS = 500; // 250 ms background CPU load per request
+const AUTO_BURN_MS = 700; // 250 ms background CPU load per request
 
 function burnCPU(ms) {
   const end = Date.now() + ms;
@@ -24,7 +24,7 @@ const server = http.createServer((req, res) => {
 
     // immediate response
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from OC3 -v2\n");
+    res.end("Hello from OC3 V1\n");
     return;
   }
 
