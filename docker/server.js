56c160dfda105bf5a2363a7e86e905fa1b8bcfe5
Sahil-401 <sahil.singhhr21@gmail.com>
2026-01-22 11:00:19 +0530

    Update server.js

--- docker/server.js
+++ docker/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const AUTO_BURN_MS = 100; // 250 ms background CPU load per request
+const AUTO_BURN_MS = 500; // 250 ms background CPU load per request
 
 function burnCPU(ms) {
   const end = Date.now() + ms;
