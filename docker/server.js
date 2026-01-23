cc85d7caf582372ff1e5d1bd70cc429e5877bbf3
Sahil-401 <sahil.singhhr21@gmail.com>
2026-01-23 14:35:54 +0530

    Update server.js

--- docker/server.js
+++ docker/server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     res.end(
       JSON.stringify(
         {
-          message: "Session Affinity Test",
+          message: "Session Connectd with Cluster-2",
           hostname: os.hostname(), // POD name
           pid: process.pid,
           requestCountOnThisPod: requestCount,
